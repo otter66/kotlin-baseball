d867b45d081f5bb4645153e23e1f224a742437f5
otter <a33397397@gmail.com>
2022-11-27 14:22:47 +0900

    style: enum클래스를 이용한 문구 정리

---
+++ src/main/kotlin/values/Notification.kt
@@ -0,0 +1,9 @@
+package values
+
+enum class Notification(val message: String) {
+    GAME_START("숫자 야구 게임을 시작합니다."),
+    REQUEST_NUMBER("숫자를 입력해주세요 : "),
+    GAME_SUCCESS("3개의 숫자를 모두 맞히셨습니다! 게임 종료"),
+    REQUEST_COMMAND("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요."),
+    CORRECT_NOTHING("낫싱")
+}
